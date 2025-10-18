45f7cb1ce2ddbc36e6034c495043c8be402d1c52
barneserella <girlthisismycodingemail@gmail.com>
2025-10-18 16:08:01 -0500

    change port

--- index.js
+++ index.js
@@ -89,6 +89,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
